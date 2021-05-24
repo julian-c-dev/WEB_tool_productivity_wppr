c64519e3ac4b307bcdc74e68d0777f0bde22827d
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-05-24 22:19:40 +0100

    Func to swap bg after selections (footer no sucs)

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,60 @@
-// This code stinks, I know, it was the first approach, It needs better way to do it.
+// This code stinks, I know, it was the first approach (I am learning), It needs to become DRY.
+
+// Changing BODY BACKGROUND with selections.
+
+const selectionColor1 = document.querySelector(".select1");
+const selectionColor2 = document.querySelector(".select2");
+const selectionColor3 = document.querySelector(".select3");
+const selectionColor4 = document.querySelector(".select4");
+const selectionColor5 = document.querySelector(".select5");
+const selectionColor6 = document.querySelector(".select6");
+
+selectionColor1.onclick = () => {
+  document.body.style.background =
+    "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)";
+  changeColor();
+};
+
+selectionColor2.onclick = () => {
+  document.body.style.background =
+    "linear-gradient(to top, #30cfd0 0%, #330867 100%)";
+};
+
+selectionColor3.onclick = () => {
+  document.body.style.background =
+    "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)";
+  changeColor();
+};
+
+selectionColor4.onclick = () => {
+  document.body.style.background =
+    "linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)";
+  changeColor();
+};
+
+selectionColor5.onclick = () => {
+  document.body.style.background = `linear-gradient(to top,
+        #dbdcd7 0%,
+        #dddcd7 24%,
+        #e2c9cc 30%,
+        #e7627d 46%,
+        #b8235a 59%,
+        #801357 71%,
+        #3d1635 84%,
+        #1c1a27 100%
+      )`;
+};
+
+selectionColor6.onclick = () => {
+  document.body.style.background =
+    "linear-gradient(to right, #434343 0%, black 100%)";
+};
+
+// Swap footer depends of Background Selection
+
+const footer = document.querySelector(".footer-light");
+
+// Adding TILES
 
 const iconPlus = document.getElementsByClassName("fa-plus-circle")[0];
 const tileAdd = document.getElementsByClassName("hide")[0];
