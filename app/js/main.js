b723cce6d6caeb25406d952a2e1538060d20ee52
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-05-25 13:26:54 +0100

    Make the site fully responsive (done!).

--- app/js/main.js
+++ app/js/main.js
@@ -59,8 +59,6 @@ const footer = document.querySelector(".footer-light");
 const iconPlus = document.getElementsByClassName("fa-plus-circle")[0];
 const tileAdd = document.getElementsByClassName("hide")[0];
 const inputs = document.getElementsByTagName("input")[1];
-const previewChange = document.getElementsByTagName("img")[3];
-console.log(previewChange);
 
 iconPlus.addEventListener("click", addingTiles);
 
@@ -68,7 +66,6 @@ function addingTiles() {
   iconPlus.classList.toggle("grey-hidden");
   tileAdd.classList.remove("hide");
   inputs.removeAttribute("disabled");
-  previewChange.src = "/images/tile2.png";
 }
 
 const iconPlus2 = document.getElementsByClassName("fa-plus-circle")[1];
@@ -81,7 +78,6 @@ function addingTiles2() {
   iconPlus2.classList.toggle("grey-hidden");
   tileAdd2.classList.remove("hide");
   inputs2.removeAttribute("disabled");
-  previewChange.src = "/images/tile3.png";
 }
 
 const iconPlus3 = document.getElementsByClassName("fa-plus-circle")[2];
@@ -94,7 +90,6 @@ function addingTiles3() {
   iconPlus3.classList.toggle("grey-hidden");
   tileAdd3.classList.remove("hide");
   inputs3.removeAttribute("disabled");
-  previewChange.src = "/images/tile4.png";
 }
 
 const iconPlus4 = document.getElementsByClassName("fa-plus-circle")[3];
@@ -107,7 +102,6 @@ function addingTiles4() {
   iconPlus4.classList.toggle("grey-hidden");
   tileAdd4.classList.remove("hide");
   inputs4.removeAttribute("disabled");
-  previewChange.src = "/images/tile5.png";
 }
 
 const iconPlus5 = document.getElementsByClassName("fa-plus-circle")[4];
@@ -119,5 +113,4 @@ iconPlus5.addEventListener("click", addingTiles5);
 function addingTiles5() {
   iconPlus5.classList.toggle("grey-hidden");
   inputs5.removeAttribute("disabled");
-  previewChange.src = "/images/tile6.png";
 }
