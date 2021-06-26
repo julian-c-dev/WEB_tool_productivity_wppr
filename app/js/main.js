c7183de2cf49d704879ed65bdb3cf6a6b948485e
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-06-26 08:43:23 +0100

    Getting Users data on the console. Refactoring.

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@
 
 //  & 1.Select your favorite background from the following options:
 
-let selectBackground = "selection1";
+let background = "selection1";
 const selectionColor1 = document.querySelector(".select1");
 const selectionColor2 = document.querySelector(".select2");
 const selectionColor3 = document.querySelector(".select3");
@@ -16,28 +16,28 @@ selectionColor1.onclick = () => {
   document.body.style.background =
     "linear-gradient(to top, #30cfd0 0%, #330867 100%)";
   document.body.style.backgroundSize = "100vw 130vh";
-  selectedBackground = "selection1";
+  background = "selection1";
 };
 
 selectionColor2.onclick = () => {
   document.body.style.background =
     "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)";
   document.body.style.backgroundSize = "100vw 130vh";
-  selectedBackground = "selection2";
+  background = "selection2";
 };
 
 selectionColor3.onclick = () => {
   document.body.style.background =
     "linear-gradient(to top, #667eea 0%, #764ba2 100%)";
   document.body.style.backgroundSize = "100vw 130vh";
-  selectedBackground = "selection3";
+  background = "selection3";
 };
 
 selectionColor4.onclick = () => {
   document.body.style.background =
     "linear-gradient(to top, #48c6ef 0%, #6f86d6 100%)";
   document.body.style.backgroundSize = "100vw 130vh";
-  selectedBackground = "selection4";
+  background = "selection4";
 };
 
 selectionColor5.onclick = () => {
@@ -52,19 +52,19 @@ selectionColor5.onclick = () => {
         #1c1a27 100%
       )`;
   document.body.style.backgroundSize = "100vw 130vh";
-  selectedBackground = "selection5";
+  background = "selection5";
 };
 
 selectionColor6.onclick = () => {
   document.body.style.background =
     "linear-gradient(to top, #434343 0%, black 100%)";
   document.body.style.backgroundSize = "100vw 130vh";
-  selectedBackground = "selection6";
+  background = "selection6";
 };
 
 // & 2 - How many tiles do you want to add to your Wallpaper?
 
-let selectedTiles = 2;
+let tiles = 0;
 
 // * images:
 const caugthSelectTwo = document.querySelector(".selectTwo");
@@ -191,7 +191,7 @@ caugthSelectTwo.onclick = () => {
   getFont.onclick = () => {
     turnFontBig();
   };
-  selectedTiles = 2;
+  tiles = 2;
 };
 
 caugthSelectFour.onclick = () => {
@@ -204,7 +204,7 @@ caugthSelectFour.onclick = () => {
   getFont.onclick = () => {
     turnFontSmall();
   };
-  selectedTiles = 4;
+  tiles = 4;
 };
 
 // & 3 - Type the titles for your tiles and add them by clicking on the plus icon:
@@ -215,6 +215,12 @@ const icon3Tile = document.getElementById("iconTile3");
 const icon4Tile = document.getElementById("iconTile4");
 const icon5Tile = document.getElementById("iconTile5");
 const icon6Tile = document.getElementById("iconTile6");
+let title1 = "";
+let title2 = "";
+let title3 = "";
+let title4 = "";
+let title5 = "";
+let title6 = "";
 
 function turnGreen(icon) {
   icon.classList.add("icon-actived");
@@ -225,49 +231,49 @@ function turnGreen(icon) {
 
 icon1Tile.onclick = () => {
   const input1Tile = document.querySelector("#tile1");
-  valueInput1Tile = input1Tile.value;
-  domTwo1Tile.innerText = valueInput1Tile;
+  title1 = input1Tile.value;
+  domTwo1Tile.innerText = title1;
   turnGreen(icon1Tile);
 };
 
 icon2Tile.onclick = () => {
   const input2Tile = document.querySelector("#tile2");
-  valueInput2Tile = input2Tile.value;
-  domTwo2Tile.innerText = valueInput2Tile;
+  title2 = input2Tile.value;
+  domTwo2Tile.innerText = title2;
   turnGreen(icon2Tile);
 };
 
 icon3Tile.onclick = () => {
   const input3Tile = document.querySelector("#tile3");
-  valueInput3Tile = input3Tile.value;
-  domFour1Tile.innerText = valueInput3Tile;
+  title3 = input3Tile.value;
+  domFour1Tile.innerText = title3;
   turnGreen(icon3Tile);
 };
 
 icon4Tile.onclick = () => {
   const input4Tile = document.querySelector("#tile4");
-  valueInput4Tile = input4Tile.value;
-  domFour2Tile.innerText = valueInput4Tile;
+  title4 = input4Tile.value;
+  domFour2Tile.innerText = title4;
   turnGreen(icon4Tile);
 };
 
 icon5Tile.onclick = () => {
   const input5Tile = document.querySelector("#tile5");
-  valueInput5Tile = input5Tile.value;
-  domFour3Tile.innerText = valueInput5Tile;
+  title5 = input5Tile.value;
+  domFour3Tile.innerText = title5;
   turnGreen(icon5Tile);
 };
 
 icon6Tile.onclick = () => {
   const input6Tile = document.querySelector("#tile6");
-  valueInput6Tile = input6Tile.value;
-  domFour4Tile.innerText = valueInput6Tile;
+  title6 = input6Tile.value;
+  domFour4Tile.innerText = title6;
   turnGreen(icon6Tile);
 };
 
 // & 4 - Select a beautiful font family for your tiles titles:
 
-let selectedFonts = "Courgette";
+let font = "Courgette";
 
 const getFont = document.getElementById("fonts");
 
@@ -275,24 +281,24 @@ function turnFontBig() {
   const tileBig = document.getElementsByClassName("tile-big");
 
   if (getFont.value === "font1") {
-    selectedFonts = "Courgette";
+    font = "Courgette";
     for (let element of tileBig) element.style.fontFamily = "Courgette";
   }
 
   if (getFont.value === "font2") {
-    selectedFonts = "Roboto";
+    font = "Roboto";
     for (let element of tileBig) element.style.fontFamily = "Roboto";
   }
   if (getFont.value === "font3") {
-    selectedFonts = "Comic Neue";
+    font = "Comic Neue";
     for (let element of tileBig) element.style.fontFamily = "Comic Neue";
   }
   if (getFont.value === "font4") {
-    selectedFonts = "PT Serif";
+    font = "PT Serif";
     for (let element of tileBig) element.style.fontFamily = "PT Serif";
   }
   if (getFont.value === "font5") {
-    selectedFonts = "Space Mono";
+    font = "Space Mono";
     for (let element of tileBig) element.style.fontFamily = "Space Mono";
   }
 }
@@ -301,34 +307,35 @@ function turnFontSmall() {
   const tileSmall = document.getElementsByClassName("tile-small");
 
   if (getFont.value === "font1") {
-    selectedFonts = "Courgette";
+    font = "Courgette";
     for (let element of tileSmall) element.style.fontFamily = "Courgette";
   }
 
   if (getFont.value === "font2") {
-    selectedFonts = "Roboto";
+    font = "Roboto";
     for (let element of tileSmall) element.style.fontFamily = "Roboto";
   }
 
   if (getFont.value === "font3") {
-    selectedFonts = "Comic Neue";
+    font = "Comic Neue";
     for (let element of tileSmall) element.style.fontFamily = "Comic Neue";
   }
 
   if (getFont.value === "font4") {
-    selectedFonts = "PT Serif";
+    font = "PT Serif";
     for (let element of tileSmall) element.style.fontFamily = "PT Serif";
   }
 
   if (getFont.value === "font5") {
-    selectedFonts = "Space Mono";
+    font = "Space Mono";
     for (let element of tileSmall) element.style.fontFamily = "Space Mono";
   }
 }
 
 // & 5 - Choose the resolution of your screen in pixels:
-let resolution = document.getElementsByName("radAnswer");
-let selectedResolution = resolution.checked;
+
+const getRadios = document.getElementById("radios");
+let resolution;
 
 // ~ BUTTONS ~
 
@@ -356,15 +363,23 @@ function previewOut() {
 
 const wallpaper_obj = document.querySelector("#generate");
 
+function showTitles(tiles) {
+  if (tiles === 0) return ["No titles selected"];
+  if (tiles === 2) return [title1, title2];
+  if (tiles === 4) return [title3, title4, title5, title6];
+}
+
 wallpaper_obj.onclick = () => {
   let user_wallpaper = {
     id: Math.floor(Math.random() * (1000 - 100) + 100) / 100,
-    selectBackground,
-    selectedTiles,
-    selectedFonts,
-    selectedResolution,
+    background,
+    tiles,
+    titles: showTitles(tiles),
+    font,
+    resolution: [...getRadios.children].find((c) => c.checked).value,
   };
-  alert(JSON.stringify(user_wallpaper));
+
+  console.log(JSON.stringify(user_wallpaper));
 };
 
 // & Btn Reset
